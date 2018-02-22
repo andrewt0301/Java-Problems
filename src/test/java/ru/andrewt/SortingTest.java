70d4b80f66e2b7a91aff70bbecbb4f4bbd75d00f
Andrei Tatarnikov <andrewt0301@gmail.com>
2018-02-22 11:08:33 +0300

    SortingTest updated

--- src/test/java/ru/andrewt/SortingTest.java
+++ src/test/java/ru/andrewt/SortingTest.java
@@ -7,16 +7,16 @@ public class SortingTest {
 
   @Test
   public void testInsertionSort() throws Exception {
-    final int[] data = new int[] {9, 1, 5, 2, 6, 3, 7, 4, 8};
+    final int[] data = new int[] {9, 1, 3, 5, 2, 6, 3, 7, 4, 2, 8};
     Sorting.insertionSort(data);
-    Assert.assertEquals(data, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+    Assert.assertEquals(data, new int[] {1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9});
   }
 
   @Test
   public void testBubbleSort() throws Exception {
-    final int[] data = new int[] {9, 1, 5, 2, 6, 3, 7, 4, 8};
+    final int[] data = new int[] {9, 1, 3, 5, 2, 6, 3, 7, 4, 2, 8};
     Sorting.bubbleSort(data);
-    Assert.assertEquals(data, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+    Assert.assertEquals(data, new int[] {1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9});
   }
 
 }
