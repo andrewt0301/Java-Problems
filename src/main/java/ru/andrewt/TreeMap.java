c50032cfe27a1b624a5f781b61c636edabbb6285
Andrei Tatarnikov <andrewt0301@gmail.com>
2018-10-18 23:58:54 +0300

    Working on TreeMap
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

---
+++ src/main/java/ru/andrewt/TreeMap.java
@@ -0,0 +1,67 @@
+package ru.andrewt;
+
+public final class TreeMap<K extends Comparable<K>, V> {
+  private static final class Node<K extends Comparable<K>, V> {
+    final K key;
+    final V value;
+
+    Node p = null;
+    Node left = null;
+    Node right = null;
+
+    Node(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+
+  private static final Node<?,?> NIL = new Node<>(null, null);
+
+  private Node<K,V> root;
+  private int size;
+
+  public TreeMap() {
+    this.root = (Node<K, V>) NIL;
+    this.size = 0;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public V get(K k) {
+    if (k == null) {
+      return null;
+    }
+
+    Node<K,V> x = root;
+    while (x != NIL && !k.equals(x.key)) {
+      if (k.compareTo(x.key) < 0) {
+        x = x.left;
+      } else {
+        x = x.right;
+      }
+    }
+    return x.value;
+  }
+
+  public V min() {
+    Node<K,V> x = root;
+    while (x.left != NIL) {
+      x = x.left;
+    }
+    return x.value;
+  }
+
+  public V max() {
+    Node<K,V> x = root;
+    while (x.right != NIL) {
+      x = x.right;
+    }
+    return x.value;
+  }
+}
