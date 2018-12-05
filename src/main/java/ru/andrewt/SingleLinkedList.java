90ba03865cabfeb3973b3f9419f3e89718d72266
andrewt0301 <andrewt0301@gmail.com>
2018-12-05 15:56:54 +0300

    Update
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/ru/andrewt/SingleLinkedList.java
+++ src/main/java/ru/andrewt/SingleLinkedList.java
@@ -1,6 +1,26 @@
+/*
+ * Copyright 2018 AndrewT (https://github.com/andrewt0301)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package ru.andrewt;
 
+/**
+ * The {@link SingleLinkedList} class implements the single-linked list data structure.
+ *
+ * @author Andrei Tatarnikov
+ */
 public final class SingleLinkedList {
+
   public static final class Node {
     public int value;
     public Node next;
@@ -11,9 +31,9 @@ public final class SingleLinkedList {
     }
   }
 
-  private Node head;
-  private Node tail;
-  private int length;
+  Node head;
+  Node tail;
+  int length;
 
   public SingleLinkedList() {
     this.head = null;
