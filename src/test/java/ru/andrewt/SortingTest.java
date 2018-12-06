76342658bbfc078d7850cbe984083333dcee4a4a
andrewt0301 <andrewt0301@gmail.com>
2018-12-06 18:49:39 +0300

    Tests updated
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/test/java/ru/andrewt/SortingTest.java
+++ src/test/java/ru/andrewt/SortingTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2018 AndrewT (https://github.com/andrewt0301)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package ru.andrewt;
 
 import org.junit.Assert;
@@ -5,6 +19,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+/**
+ * Tests for the {@link Sorting} class.
+ *
+ * @author Andrei Tatarnikov
+ */
 public class SortingTest {
   static final int[] SORTED = new int[] {1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9};
   static final int[] UNSORTED = new int[] {9, 1, 3, 5, 2, 6, 3, 7, 4, 2, 8};
@@ -55,6 +74,23 @@ public class SortingTest {
   public void testRadixSort() {
     final int[] data = Arrays.copyOf(UNSORTED, UNSORTED.length);
     Sorting.radixSort(data);
+    Assert.assertArrayEquals(SORTED, data);
+  }
+
+  @Test
+  public void testPartitionWithMask() {
+    final int[] data = Arrays.copyOf(UNSORTED, UNSORTED.length);
+
+    Sorting.partitionWithMask(data, 0b1);
+    Assert.assertArrayEquals(new int[] {2, 6, 4, 2, 8, 9, 1, 3, 5, 3, 7}, data);
+
+    Sorting.partitionWithMask(data, 0b10);
+    Assert.assertArrayEquals(new int[] {4, 8, 9, 1, 5, 2, 6, 2, 3, 3, 7}, data);
+
+    Sorting.partitionWithMask(data, 0b100);
+    Assert.assertArrayEquals(new int[] {8, 9, 1, 2, 2, 3, 3, 4, 5, 6, 7}, data);
+
+    Sorting.partitionWithMask(data, 0b1000);
     Assert.assertArrayEquals(SORTED, data);
   }
 
