bfde90eb8ca92e791538aae09fa06532adc37e77
andrewt0301 <andrewt0301@gmail.com>
2019-03-22 15:49:25 +0300

    Updated sorting algorithms.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/ru/andrewt/Sorting.java
+++ src/main/java/ru/andrewt/Sorting.java
@@ -14,7 +14,7 @@ public final class Sorting {
   /**
    * Sorts an array of integers into ascending numerical order using the insertion sort algorithm.
    * <p>
-   * Time complexity: {@code Θ(N**2)}, where N is the length of the array.
+   * Time complexity: {@code Θ(N^2)}, where N is the length of the array.
    * <p/>
    * @param data Array to be sorted.
    */
@@ -35,8 +35,8 @@ public final class Sorting {
 
   public static void bubbleSort(final int[] data) {
 
-    for (int i = 0; i < data.length - 1; i++) {
-      for (int j = data.length - 1; j > i; j--) {
+    for (int i = 0; i < data.length - 1; ++i) {
+      for (int j = data.length - 1; j > i; --j) {
         final int k = j - 1;
         if (data[j] < data[k]) {
           ArrayUtils.swap(data, j, k);
@@ -45,11 +45,10 @@ public final class Sorting {
     }
   }
 
-
   /**
    * Sorts an array of integers into ascending numerical order using the selection sort algorithm.
    * <p>
-   * Time complexity: {@code Θ(N**2)}, where N is the length of the array.
+   * Time complexity: {@code Θ(N^2)}, where N is the length of the array.
    * <p/>
    * @param data Array to be sorted.
    */
@@ -99,36 +98,64 @@ public final class Sorting {
     return split;
   }
 
+  /**
+   * Sorts an array of integers into ascending numerical order using the merge sort algorithm.
+   * <p>
+   * Time complexity: {@code Θ(N lg N)}, where N is the length of the array.
+   * <p/>
+   * @param data Array to be sorted.
+   */
   public static void mergeSort(final int[] data) {
     mergeSort(data, 0, data.length - 1);
   }
 
+  /**
+   * Sorts the specified range in an array of integers into ascending
+   * numerical order using the merge sort algorithm.
+   * <p>
+   * Time complexity: {@code Θ(N lg N)}, where N is the length of the sorted range.
+   * <p/>
+   * @param data Array to be sorted.
+   * @param start Range start index (included).
+   * @param end Range end index (included).
+   */
   public static void mergeSort(final int[] data, final int start, final int end) {
 
     if (start < end) {
       final int split = (start + end) / 2;
+
       mergeSort(data, start, split);
       mergeSort(data, split + 1, end);
+
       merge(data, start, split, end);
     }
   }
 
+
+  /**
+   * Merges two adjacent sorted ranges in an integer array into a single sorted range.
+   *
+   * @param data Array to be merged.
+   * @param start Start position of the first range (included).
+   * @param split End position of the first range, followed by start position of the second range
+   *             (included).
+   * @param end End position of the second range (included).
+   */
   private static void merge(int[] data, int start, int split, int end) {
 
+    // Creates a copy of the first (left) range.
     final int leftLen = split - start + 1;
     final int[] leftData = new int[leftLen + 1];
-    for (int index = 0; index < leftLen; index++) {
-      leftData[index] = data[start + index];
-    }
+    System.arraycopy(data, start, leftData, 0, leftLen);
     leftData[leftLen] = Integer.MAX_VALUE;
 
+    // Creates a copy of the second (right) range.
     final int rightLen = end - split;
     final int[] rightData = new int[rightLen + 1];
-    for (int index = 0; index < rightLen; index++) {
-      rightData[index] = data[split + index + 1];
-    }
+    System.arraycopy(data, split + 1, rightData, 0, rightLen);
     rightData[rightLen] = Integer.MAX_VALUE;
 
+    // Merges the two ranges and saves the result to the data array.
     for (int index = start, leftIndex = 0, rightIndex = 0; index <= end; ++index) {
       final int left = leftData[leftIndex];
       final int right = rightData[rightIndex];
