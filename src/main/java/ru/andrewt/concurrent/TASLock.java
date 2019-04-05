db6a4106e9f9ae8603ef0e20017e6899620097bf
andrewt0301 <andrewt0301@gmail.com>
2019-04-05 15:15:52 +0300

    Small update.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/ru/andrewt/concurrent/TASLock.java
+++ src/main/java/ru/andrewt/concurrent/TASLock.java
@@ -3,6 +3,11 @@ package ru.andrewt.concurrent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Lock based on the test-and-set (TAS) instruction.
+ *
+ * {@see <a https://en.wikipedia.org/wiki/Test-and-set"/>}
+ */
 public class TASLock implements Lock {
   private final AtomicBoolean state = new AtomicBoolean(false);
 
--- src/main/java/ru/andrewt/concurrent/TTASLock.java
+++ src/main/java/ru/andrewt/concurrent/TTASLock.java
@@ -3,13 +3,18 @@ package ru.andrewt.concurrent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Lock based on the test and test-and-set (TTAS) instruction.
+ *
+ * {@see <a https://en.wikipedia.org/wiki/Test_and_test-and-set"/>}
+ */
 public class TTASLock implements Lock {
   private final AtomicBoolean state = new AtomicBoolean(false);
 
   @Override
   public void lock() {
     while (true) {
-      while (state.get()) {}
+      while (state.get())
       if (!state.getAndSet(true)) {
         return;
       }
