60759f32571e2daf96bb7b8e456c4d6b947271de
andrewt0301 <andrewt0301@gmail.com>
2019-07-15 20:05:25 +0300

    Strings updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/ru/andrewt/Strings.java
+++ src/main/java/ru/andrewt/Strings.java
@@ -16,12 +16,52 @@ public final class Strings {
    *                  the complexity of O(N^2).
    * <p/>
    * @param text Text.
+   * @param start Starting index for search.
    * @param str String to be searched.
-   * @return Start position of the string in the text or -1 if no occurrence
-   *         of the string is found.
+   * @return Start position of the string in the text or -1 if no occurrence of the string is found.
    */
-  public static int naiveSearch(String text, String str) {
-    // TODO
+  public static int naiveSearch(String text, int start, String str) {
+
+    final int textLen = text.length();
+    final int strLen = str.length();
+
+    if (start >= textLen) {
+      return (strLen == 0 ? textLen : -1);
+    }
+    if (start < 0) {
+      start = 0;
+    }
+    if (strLen == 0) {
+      return start;
+    }
+
+    final char ch = str.charAt(0);
+    final int max = textLen - strLen;
+
+    for (int i = start; i <= max; ++i) {
+
+      while (i <= max && text.charAt(i) != ch) {
+        ++i;
+      }
+
+      if (i <= max) {
+        int j = i + 1;
+        int k = 1;
+
+        while (k < strLen) {
+          if (text.charAt(j) != str.charAt(k)) {
+            break;
+          }
+          ++j;
+          ++k;
+        }
+
+        if (k == strLen) {
+          return i;
+        }
+      }
+    }
+
     return -1;
   }
 
---
+++ src/test/java/ru/andrewt/StringsTest.java
@@ -0,0 +1,32 @@
+
+package ru.andrewt;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for the {@link Strings} class.
+ *
+ * @author Andrei Tatarnikov
+ */
+public class StringsTest {
+
+  @Test
+  public void testNaiveSearch() {
+    final String text = "This is a strange test string!";
+
+    Assert.assertEquals(0,  Strings.naiveSearch(text, 0, "This"));
+    Assert.assertEquals(2,  Strings.naiveSearch(text, 0, "is"));
+    Assert.assertEquals(2,  Strings.naiveSearch(text, -1,"is"));
+    Assert.assertEquals(23, Strings.naiveSearch(text, 0, "stri"));
+    Assert.assertEquals(26, Strings.naiveSearch(text, 0, "ing!"));
+
+    Assert.assertEquals(5,  Strings.naiveSearch(text, 3, "is"));
+    Assert.assertEquals(-1, Strings.naiveSearch(text, text.length() - 1, "ing!"));
+
+    Assert.assertEquals(-1, Strings.naiveSearch("", 3, "is"));
+    Assert.assertEquals(0,  Strings.naiveSearch("", 0, ""));
+    Assert.assertEquals(5,  Strings.naiveSearch(text, 5, ""));
+  }
+
+}
